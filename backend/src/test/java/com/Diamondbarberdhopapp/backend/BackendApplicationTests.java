c0abec2dffcb01c615d8159ce7837e5985263606
HanadAhmedM <Hanadahmed1414@gmail.com>
2026-05-08 22:41:28 +0200

    Initial Spring Boot backend setup

---
+++ backend/src/test/java/com/Diamondbarberdhopapp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Diamondbarberdhopapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
